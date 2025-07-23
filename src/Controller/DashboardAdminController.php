56d001c202ed290d79d61b8ecbe54911d8acf955
SabrinaBenoudiba <sabrina.benoudiba@gmail.com>
2025-07-23 17:22:23 +0200

    Exo_CrudEtRoles

---
+++ src/Controller/DashboardAdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class DashboardAdminController extends AbstractController
+{
+    #[Route('/dashboardAdmin', name: 'app_dashboard_admin')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function dashboardAdmin(): Response
+    {
+        return $this->render('dashboard_admin/index.html.twig', [
+            'controller_name' => 'DashboardAdminController',
+        ]);
+    }
+}
---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomePageController extends AbstractController
+{
+    #[Route('/', name: 'app_home_page')]
+    public function homePage(): Response
+    {
+        return $this->render('home_page/homePage.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+}
+{
+
+}
