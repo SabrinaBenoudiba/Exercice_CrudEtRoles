0ce8a3c5ef0960c9819324321d2a6fc1e2319786
SabrinaBenoudiba <sabrina.benoudiba@gmail.com>
2025-07-24 11:12:16 +0200

    maj

--- src/Controller/AdminHomePageController.php
+++ src/Controller/AdminHomePageController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserUpdateType;
+use App\Form\AdminUpdateType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,26 +12,27 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-final class UserHomePageController extends AbstractController
+// #[Route('/admin')]
+final class AdminHomePageController extends AbstractController
 {
-    #[Route('/userHomePage', name: 'app_userHomePage')]
-    #[IsGranted("ROLE_USER")]
-    public function userHomePage(UserRepository $userRepo): Response
+    #[Route('/adminHomePage', name: 'app_adminHomePage')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function HomePage(UserRepository $userRepo): Response
     {
         
-        $datas = $userRepo->findAll();
-        return $this->render('user_home_page/userHomePage.html.twig', [
-            'controller_name' => 'userHomePageController',
+        $datas = $userRepo->findall();
+        return $this->render('admin_home_page/adminHomePage.html.twig', [
+            'controller_name' => 'adminHomePageController',
             'datas' => $datas,
         ]);
     }
 
-    #[Route('/userHomePage/{id}', name: 'app_update')]
-    #[IsGranted("ROLE_USER")] 
-    public function update_form($id, Request $request, EntityManagerInterface $entityManager, UserRepository $userRepo): Response 
+    #[Route('/adminHomePage/{id}', name: 'app_update_admin')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function update_form($id, Request $request, EntityManagerInterface $entityManager,UserRepository $userRepo ): Response 
     {
         $crud = $entityManager->getRepository(User::class)->find($id);
-        $form = $this->createForm(UserUpdateType::class, $crud); 
+        $form = $this->createForm(AdminUpdateType::class, $crud); 
         $form->handleRequest($request); 
         if ( $form->isSubmitted() && $form->isValid()){ 
             $entityManager->persist($crud); 
@@ -39,20 +40,18 @@ final class UserHomePageController extends AbstractController
 
             $this->addFlash('notice', 'Modification réussi !!');
 
-            return $this->redirectToRoute('app_userHomePage'); 
+            return $this->redirectToRoute('app_adminHomePage'); 
         }
+         $datas = $userRepo->findall();
 
-        $datas = $userRepo->findAll();
-
-         return $this->render('user_home_page/updateUserForm.html.twig', [
+         return $this->render('admin_home_page/updateAdminForm.html.twig', [
             'form' => $form->createView(),
             'datas' => $datas,
-
         ]);
     }
 
-    #[Route('/delete/{id}', name: 'app_delete')]
-    #[IsGranted("ROLE_USER")]
+    #[Route('/delete/{id}', name: 'app_delete_admin')]
+    #[IsGranted("ROLE_ADMIN")]
     public function delete($id, EntityManagerInterface $entityManager): Response 
     {
         $del = $entityManager->getRepository(User::class)->find($id);
@@ -65,5 +64,4 @@ final class UserHomePageController extends AbstractController
             return $this->redirectToRoute('app_home_page'); 
       
     }
-       
 }
